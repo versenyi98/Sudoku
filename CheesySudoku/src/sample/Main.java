56305c2b5e6f380f15b3d9a565c08360acbb560c
versenyi98 <versenyi98@gmail.com>
2019-10-25 21:43:10 +0200

    S30 Add basic Sudoku generator
    The generator generates basic Sudoku without any special rules, other than the bases of Sudoku.

--- CheesySudoku/src/sample/Main.java
+++ CheesySudoku/src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import controllers.*;
+
 public class Main extends Application {
 
     @Override
@@ -14,6 +16,18 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        BaseSudokuGenerator generator = new BaseSudokuGenerator();
+
+        generator.setCellWidth(3);
+        generator.setCellHeight(3);
+        generator.setCellNumbersHorizontal(3);
+        generator.setCellNumbersVertical(3);
+
+        generator.fillBlankTable();
+
+        generator.generate();
+        generator.printSudoku();
     }
 
 
