ea050b64d6d7386bb36fcfb29c4107a72fff7075
Ádám <adamszilard7@gmail.com>
2019-11-07 14:18:33 +0100

    S50 add error handling

---
+++ backend/errors.php
@@ -0,0 +1,8 @@
+<?php
+    $error_out_code = "errorCode";
+    $error_out_msg = "errorMessage";
+
+    $error_database = 1;
+    $error_no_auth = 2;
+    $error_wrong_auth = 3;
+?>
--- backend/login.php
+++ backend/login.php
@@ -1,26 +1,38 @@
 <?php
     header('Content-type:application/json');
     include_once('util.php');
+    include_once('errors.php');
 
     $conn = getDBConnection();
     if($conn === false)
     {
         http_response_code(500);
-        echo "{\"errorMessage\": \"A connection to the database server could not be established.\"";
+        $error = array(
+            $error_out_code => $error_database,
+            $error_out_msg => "A connection to the database server could not be established."
+        );
+        
+        die(json_encode($error));
     }
 
     // login is only called to get a random salt
     if(isset($_POST['getsalt']))
     {
-        $salt = getSalt();
-        echo "{\"salt\":\"" . $salt . "\"}";
-        die();
+        $output = array("salt" => getSalt());
+        
+        $conn->close();
+        die(json_encode($output));
     }
 
-    if(!isset($_POST['username']) || !isset($_POST['hash']) || !isset($_POST['salt']))
+    if(!isset($_POST['username']) || !isset($_POST['hash']))
     {
         http_response_code(400);
-        die("{\"errorMessage\": \"A username, password hash, or password salt hasn't been provided.\"}");
+        $error = array(
+            $error_out_code => $error_no_auth,
+            $error_out_msg => "A username or password hash hasn't been provided."
+        );
+        $conn->close();
+        die(json_encode($error));
     }
 
 
