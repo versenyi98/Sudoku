450a8132288765acce251b100c0abe07d425d878
Ádám <adamszilard7@gmail.com>
2019-11-11 14:34:49 +0100

    S50 add authentication data check and remove expectation of client side hashing

--- backend/login.php
+++ backend/login.php
@@ -24,16 +24,42 @@
         die(json_encode($output));
     }
 
-    if(!isset($_POST['username']) || !isset($_POST['hash']))
+	// a username or password hasn't been provided
+    if(!isset($_POST['username']) || !isset($_POST['password']))
     {
         http_response_code(400);
         $error = array(
             $error_out_code => $error_no_auth,
-            $error_out_msg => "A username or password hash hasn't been provided."
+            $error_out_msg => "A username or password hasn't been provided."
         );
         $conn->close();
         die(json_encode($error));
     }
 
+	// try to get the password hash associated with this username
+	$passhash = $conn->query("SELECT PasswordHash, PasswordSalt FROM User WHERE Name = '" . $_POST["username"] . "';")->fetch_row();
+	// there is no such username
+	if($passhash === null)
+	{
+		http_response_code(401);
+		$error = array(
+			$error_out_code => $error_wrong_auth,
+			$error_out_msg => "The username provided doesn't exist."
+		);
+		$conn->close();
+		die(json_encode($error));
+	}
 
+	// get the hash of the password sent, and check if it matches with our hash
+	$clientPassHash = hash("SHA256", $_POST['password'] + $passhash[1]);
+	if($passhash != $clientPassHash)
+	{
+		http_response_code(401);
+		$error = array(
+			$error_out_code => $error_wrong_auth,
+			$error_out_msg => "The password provided is incorrect"
+		);
+		$conn->close();
+		die(json_encode($error));
+	}
 ?>
