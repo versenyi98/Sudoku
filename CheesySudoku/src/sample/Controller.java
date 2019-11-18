f279e7ac074b245ba11eec5ec52b854b253f8e30
Ádám <adamszilard7@gmail.com>
2019-11-18 13:30:17 +0100

    S58 add main menu and new game to prototype

--- CheesySudoku/src/sample/Controller.java
+++ CheesySudoku/src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    public GridPane main_menu_grid;
+    public Button new_game_button;
+    public Button exit_button;
+
+    public void setMenuClicks()
+    {
+        exit_button.setOnMouseClicked((MouseEvent e) -> {
+            Platform.exit();
+        });
+    }
 }
--- CheesySudoku/src/sample/Main.java
+++ CheesySudoku/src/sample/Main.java
@@ -2,20 +2,38 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import controllers.*;
 
+import java.util.HashMap;
+
 public class Main extends Application {
+
+    private HashMap<String, Scene> scenes = new HashMap<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        scenes.put("mainmenu", new Scene(root, 300, 275));
+
+        primaryStage.setTitle("Cheesy Sudoku - every sudoku game in one place");
+        primaryStage.setScene(scenes.get("mainmenu"));
         primaryStage.show();
+
+        Controller c = loader.getController();
+        c.setMenuClicks();
+        for (Node menubutton : c.main_menu_grid.getChildren()) {
+            GridPane.setHalignment(menubutton, HPos.CENTER);
+        }
+
 
         BaseSudokuGenerator generator = new IrregularSudokuGenerator();
 
@@ -26,6 +44,16 @@ public class Main extends Application {
 
         generator.generate(30);
         generator.printSudoku();
+    }
+
+    private void goToNewGameScene() {
+        Scene s;
+        if(scenes.containsKey("newgame")) {
+            s = scenes.get("newgame");
+        }
+        else {
+
+        }
     }
 
 
