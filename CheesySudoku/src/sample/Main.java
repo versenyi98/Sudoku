0edd850f6440ac978d3fe15caca89b1f1805375b
Ádám Szilárd <adamszilard7@protonmail.com>
2019-12-02 10:24:44 +0100

    S38 style menu and add new game button functionality

--- CheesySudoku/src/sample/Main.java
+++ CheesySudoku/src/sample/Main.java
@@ -11,13 +11,16 @@ public class Main extends Application {
     public static final FXMLLoader MAIN_LOADER = new FXMLLoader();
     public static Stage mainStage = new Stage();
 
+    public static int GAME_WIDTH    = 300,
+                      GAME_HEIGHT   = 310;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         mainStage = primaryStage;
-        MAIN_LOADER.setLocation(getClass().getResource("fxml/game.fxml"));
+        MAIN_LOADER.setLocation(getClass().getResource("fxml/menu.fxml"));
         Parent root = MAIN_LOADER.load();
-        Scene scene = new Scene(root, 300, 310);
-        scene.getStylesheets().add("sample/css/gameStyle.css");
+        Scene scene = new Scene(root, GAME_WIDTH, GAME_HEIGHT);
+        scene.getStylesheets().add("sample/css/menuStyle.css");
         mainStage.setResizable(false);
         mainStage.setTitle("CheesySudoku");
         mainStage.setScene(scene);
